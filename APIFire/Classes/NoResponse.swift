ba0c1179356bbd7d157e885f96cd0b6ec41df463
Zack Sheppard <zack@zacksheppard.com>
2020-08-09 22:58:39 -0400

    Add the NoResponse utility type

---
+++ APIFire/Classes/NoResponse.swift
@@ -0,0 +1,11 @@
+//
+//  NoResponse.swift
+//  Alamofire
+//
+//  Created by Zack Sheppard on 8/9/20.
+//
+
+import Foundation
+
+/// A helper type for an API Endpoint whose response you don't care about.
+public struct NoResponse: Decodable {}
